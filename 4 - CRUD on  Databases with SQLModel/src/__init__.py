218b8a752e0c60c51f9b7a12df43ff26715024af
Sayyed Aliza Faruk <aliza.sayyed@outlook.com>
2025-11-03 00:53:43 +0530

    Add files via upload

---
+++ 4 - CRUD on  Databases with SQLModel/src/__init__.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from src.db.main import init_db
+from src.books.routes import router
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print('Server is starting...')
+    await init_db()
+    yield
+    print('Server has stopped..')
+
+
+app = FastAPI(
+    lifespan=life_span
+)
+
+app.include_router(router, prefix="/books")
---
+++ 4 - CRUD on  Databases with SQLModel/src/db/main.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.asyncio import AsyncEngine
+from src.config import Config
+from sqlmodel import SQLModel
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+engine = AsyncEngine(create_engine(
+    url=Config.DATABASE_URL,
+    echo=True
+))
+
+async def init_db():
+    async with engine.begin() as conn:
+        # statement = text("SELECT 'hello'")
+        # result = await conn.execute(statement)
+    
+        # print(result.all())
+        from src.books.models import Book
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=engine,
+        class_=AsyncSession,
+        expire_on_commit=False
+    )
+
+    async with Session() as session:
+        yield session
